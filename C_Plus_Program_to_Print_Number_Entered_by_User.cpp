e735cd5aa4f860009521cc8b47b01992c272661a
name <lyhayho97@gmail.com>
2022-08-09 22:15:11 +0700

    C++ Program to Print Number Entered by User

---
+++ C_Plus_Program_to_Print_Number_Entered_by_User.cpp
@@ -0,0 +1,13 @@
+//https://www.programiz.com/cpp-programming/examples/read-print-integer
+
+#include <iostream>
+using namespace std;
+int main(){
+    int number_input;
+    cout << "input number: ";
+    cin >> number_input;
+
+    cout <<"number enter: " << number_input << endl;
+
+    return 0;
+}
