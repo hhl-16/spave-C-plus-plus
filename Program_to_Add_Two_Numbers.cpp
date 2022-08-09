bcbb05faaeb2877fc44bd65c9ed813bdbe8b61f3
name <lyhayho97@gmail.com>
2022-08-09 22:20:01 +0700

    C++ Program to Add Two Numbers

---
+++ Program_to_Add_Two_Numbers.cpp
@@ -0,0 +1,14 @@
+//https://www.programiz.com/cpp-programming/examples/add-numbers
+
+#include <iostream>
+using namespace std;
+int main(){
+    int add;
+    int number1,number2;  
+    cout << "input 2 number: ";
+    cin >> number1 >>number2;
+
+    cout <<"add two numbers: " << number1 + number2;
+
+    return 0;
+}
