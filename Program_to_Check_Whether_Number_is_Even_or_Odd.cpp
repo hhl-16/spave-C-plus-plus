8f413cc88b92e9f3ad98e24713efa041ad0d9dbf
name <lyhayho97@gmail.com>
2022-08-12 22:10:49 +0700

    C++ Program to Check Whether Number is Even or Odd

---
+++ Program_to_Check_Whether_Number_is_Even_or_Odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number ;
+    cout << "input number: ";
+    cin >> number;
+    if(number%2!=0){
+        cout<<"number odd"<<endl;
+    }
+    else {
+        cout<<"number even"<<endl;
+    }
+
+
+
+    return 0;
+}
