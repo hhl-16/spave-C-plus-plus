b72ce9a60bf20c9b12b55207425acb4b460cd235
name <lyhayho97@gmail.com>
2022-08-19 10:20:03 +0700

    C++ Program to Multiply two Numbers

---
+++ Multiply_two_Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number1,number2;   
+    int s;
+
+    cout <<"enter number1: ";
+    cin >> number1;
+    cout << "enter number2: ";
+    cin >> number2;
+
+    cout << number1*number2 << endl;
+}
